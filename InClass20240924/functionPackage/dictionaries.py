7520e6e50d1b6e4833efe6c7c39e024218b9be92
ap1805 <aryanjpatel2003@gmail.com>
2024-09-24 08:57:47 -0400

    Made changes during class

--- InClass20240924/functionPackage/dictionaries.py
+++ InClass20240924/functionPackage/dictionaries.py
@@ -1,1 +1,49 @@
 # dictionaries.py
+
+
+from xml.dom.expatbuilder import makeBuilder
+
+
+def demo():
+    '''
+    Demonstrate basic dictionary stuff
+    '''
+    myDictionary = {"Cincinnati":"Bearcats", "Xavier":"Musketeers", "NKU":"Norse", "UC Clermont":"Cougars"}
+    print(myDictionary)
+    
+    # Iterate over the dictionary by key
+    for key in myDictionary.keys():
+        print(key)
+        
+    # Iterate by key/value
+    for item in myDictionary.items():
+        print(item)
+        
+    # Iterate by value
+    for value in myDictionary.values():
+        print(value)
+        
+    # Add a key/value pair to the dictionary
+    myDictionary["Michigan State"] = "Spartans"
+    
+    print(len(myDictionary))
+    
+    # Cause an exception. Add try / except logic to
+    # gracefully handle this
+    
+    try:    
+     print(myDictionary["Ohio State"])
+    except:
+       print("Ohio State is an invalid key")
+       
+    # Remove Xavier by key and print the entire dictionary
+    myDictionary.pop("Xavier")   
+    print(myDictionary)
+    
+    # Create another dictionary called newTeams.
+    # Add several key/value pairs
+    # Combine that with myDictionary and print the results
+    newTeams = {"Ohio State":"Buckeyes","Kentucky":"Wildcats","Michigan":"Wolverines", "Indiana":"Hoosiers"}
+    myDictionary.update(newTeams)
+    print(myDictionary)
+    
